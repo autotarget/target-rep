5ff1c875fce83b3e5822874b18287451a47829bc
autotarget <autotarget33@gmail.com>
2022-05-04 10:18:39 +0300

    пример

---
+++ laravel.music/resources/views/inc/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="container-xxl">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Главная</a></li>
+      <li class="nav-item"><a href="/about" class="nav-link px-2 text-muted">О нас</a></li>
+      <li class="nav-item"><a href="/contact" class="nav-link px-2 text-muted">Контакты</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2021 Company, Inc</p>
+  </footer>
+</div>
